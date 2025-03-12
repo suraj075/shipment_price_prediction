58b22bcba8d138c2c369ab0e550f1e5a8103339d
Suraj kumar <kumarsuraj07558@gmail.com>
2025-03-12 15:30:20 +0530

    first

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='shipments',
+    version='0.0.1',
+    author="suraj",
+    author_email="kumarsuraj07553@gmail.com",
+    packages= find_packages(),
+    install_requires = []
+)
